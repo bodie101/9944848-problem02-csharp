442d1db10f3b641fc476ee2fa7fe28ec151ea627
bodie101 <keysbw@g.toiohomai.ac.nz>
2017-09-04 11:54:39 +1200

    problem2 done

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace problem02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start the program with Clear();
+            Console.Clear();
+            
+            Console.WriteLine("Please type in a number less than 100"); 
+         var input1 = Console.ReadLine(); 
+
+         var number1 = 0;  
+         var isNumber = int.TryParse(input1, out number1);
+
+         if(isNumber) {
+
+             var counter = 12;
+             if (number1 < 100)
+
+
+             for (var i = 0; i < counter; i++)
+             {
+                 var a = i + 1;
+                 Console.WriteLine($"{input1} / {a} = {number1 / a} ");
+             }
+
+         }
+         else {
+             Console.WriteLine("please type in a number.");
+         }
+            
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+    }
+}
